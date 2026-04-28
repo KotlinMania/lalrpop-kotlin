5976fa1f73564b7e33e084ab9ba36e7dbcc0f969
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 12:21:23 -0700

    Set.kt: replace wrapper class with typealias to BTreeSet to mirror Rust pub type

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
@@ -4,14 +4,8 @@ package io.github.kotlinmania.lalrpop.collections
 import io.github.kotlinmania.btree.BTreeSet
 
 /** As [Map], but for sets. */
-class Set<K : Comparable<K>>(
-    private val inner: BTreeSet<K> = BTreeSet(),
-) : MutableSet<K> by inner {
-    companion object {
-        fun <K : Comparable<K>> default(): Set<K> = Set()
-    }
-}
+typealias Set<K> = BTreeSet<K>
 
 fun <K : Comparable<K>> set(): Set<K> {
-    return Set.default<K>()
+    return Set<K>()
 }
