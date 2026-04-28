0f27cf7eee479860e8940f5c15d66958f9d8bebb
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 10:08:04 -0700

    Revert "Tls.kt: drop RefCell shim for parity"
    This reverts commit 493db50615facebdc2abfd930e22356029be2ee6.

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lr1/Tls.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lr1/Tls.kt
@@ -8,31 +8,45 @@ package io.github.kotlinmania.lalrpop.lr1
 import io.github.kotlinmania.lalrpop.grammar.repr.TerminalSet
 
 private object TERMINALS {
-    var value: TerminalSet? = null
+    private val cell: RefCell<TerminalSet?> = RefCell(null)
+    fun <R> with(op: (RefCell<TerminalSet?>) -> R): R = op(cell)
 }
 
+private class RefCell<T>(private var value: T) {
+    fun borrow(): T = value
+    fun borrowMut(): RefCell<T> = this
+    fun set(other: T) {
+        value = other
+    }
+    fun replace(other: T): T {
+        val old = value
+        value = other
+        return old
+    }
+}
+
+private fun <T> RefCell<T?>.take(): T? = this.replace(null)
+
 class Lr1Tls private constructor(
-    private var oldValue: TerminalSet?,
+    private val oldValue: RefCell<TerminalSet?>,
 ) : AutoCloseable {
 
-    private fun drop() {
-        TERMINALS.value = oldValue
-        oldValue = null
+    fun drop() {
+        TERMINALS.with { s -> s.borrowMut().set(this.oldValue.take()) }
     }
 
     override fun close() = drop()
 
     companion object {
         fun install(terminals: TerminalSet): Lr1Tls {
-            val oldValue = TERMINALS.value
-            TERMINALS.value = terminals
-            return Lr1Tls(oldValue)
+            val oldValue = TERMINALS.with { s -> s.borrowMut().replace(terminals) }
+            return Lr1Tls(RefCell(oldValue))
         }
 
         fun <RET> with(
             op: (TerminalSet) -> RET,
         ): RET {
-            return op(TERMINALS.value ?: error("LR1 TLS not installed"))
+            return TERMINALS.with { s -> op(s.borrow() ?: error("LR1 TLS not installed")) }
         }
     }
 }
