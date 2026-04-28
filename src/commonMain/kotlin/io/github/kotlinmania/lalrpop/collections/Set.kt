b8b5a817365421bc534def0cbde3843cc5f688f2
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 11:11:59 -0700

    Set.kt: use BTreeSet directly

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
@@ -3,21 +3,7 @@ package io.github.kotlinmania.lalrpop.collections
 
 import io.github.kotlinmania.btree.BTreeSet
 
-/** As [Map], but for sets. */
-class Set<K : Comparable<K>> private constructor(
-    private val inner: BTreeSet<K>,
-) : MutableSet<K> by inner {
-    fun iter(): Iterator<K> = inner.iterator()
-
-    fun len(): Int = inner.size
-
-    fun insert(value: K): Boolean = inner.insert(value)
-
-    companion object {
-        fun <K : Comparable<K>> default(): Set<K> = Set(BTreeSet())
-    }
-}
-
-fun <K : Comparable<K>> set(): Set<K> {
-    return Set.default<K>()
+/** As [io.github.kotlinmania.lalrpop.collections.map], but for sets. */
+fun <K : Comparable<K>> set(): BTreeSet<K> {
+    return BTreeSet()
 }
