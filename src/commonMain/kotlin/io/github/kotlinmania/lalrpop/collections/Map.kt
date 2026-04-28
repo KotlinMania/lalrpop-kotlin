f45a4c40626962f6ebe3581150654eecc70a6f97
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 11:16:00 -0700

    Map.kt: wrapper class for deterministic map

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Map.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Map.kt
@@ -5,12 +5,20 @@ import io.github.kotlinmania.btree.BTreeMap
 
 /**
  * In general, we avoid coding directly against any particular map,
- * but rather build against [BTreeMap] (and [map] to construct
+ * but rather build against [Map] (and [map] to construct
  * an instance). This should be a deterministic map, such that two
  * runs of LALRPOP produce the same output, but otherwise it doesn't
  * matter much. I would probably prefer to use [HashMap] with an
  * alternative hasher, but that's not stable.
  */
-fun <K : Comparable<K>, V> map(): BTreeMap<K, V> {
-    return BTreeMap()
+class Map<K : Comparable<K>, V> private constructor(
+    private val inner: BTreeMap<K, V>,
+) : MutableMap<K, V> by inner {
+    companion object {
+        fun <K : Comparable<K>, V> default(): Map<K, V> = Map(BTreeMap())
+    }
+}
+
+fun <K : Comparable<K>, V> map(): Map<K, V> {
+    return Map.default()
 }
