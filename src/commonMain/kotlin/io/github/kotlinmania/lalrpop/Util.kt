2466927f5c46e24d7cb779625447b68064ee8a12
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 06:12:21 -0700

    Util.kt: split fmt() out of toString() for symbol parity
    Display::fmt impls now have direct fmt(StringBuilder) method counterparts
    in Sep/Escape/Prefix; toString() delegates to fmt(). Function-body score
    0.13 -> 0.71.

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/Util.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/Util.kt
@@ -2,43 +2,51 @@
 package io.github.kotlinmania.lalrpop
 
 class Sep<S>(val sep: String, val vec: Iterable<S>) {
-    override fun toString(): String = buildString {
+    fun fmt(fmt: StringBuilder) {
         val elems = vec.iterator()
         if (elems.hasNext()) {
             val elem = elems.next()
-            append(elem.toString())
+            fmt.append(elem.toString())
             for (next in elems) {
-                append(sep)
-                append(next.toString())
+                fmt.append(sep)
+                fmt.append(next.toString())
             }
         }
     }
+
+    override fun toString(): String = buildString { fmt(this) }
 }
 
 class Escape<S : Any>(val value: S) {
-    override fun toString(): String = buildString {
+    fun fmt(fmt: StringBuilder) {
         val tmp = value.toString()
         for (c in tmp) {
             when (c) {
-                in 'a'..'z', in '0'..'9', in 'A'..'Z' -> append(c)
-                '_' -> append("__")
+                in 'a'..'z', in '0'..'9', in 'A'..'Z' -> fmt.append(c)
+                '_' -> fmt.append("__")
                 else -> {
-                    append('_')
-                    append(c.code.toString(16))
+                    fmt.append('_')
+                    fmt.append(c.code.toString(16))
                 }
             }
         }
     }
+
+    override fun toString(): String = buildString { fmt(this) }
 }
 
 class Prefix<S>(val prefix: String, val vec: Iterable<S>) {
-    override fun toString(): String = buildString {
+    fun fmt(fmt: StringBuilder) {
         for (elem in vec) {
-            append(prefix)
-            append(elem.toString())
+            fmt.append(prefix)
+            fmt.append(elem.toString())
         }
     }
+
+    override fun toString(): String = buildString { fmt(this) }
 }
 
 /** Strip leading and trailing whitespace. */
-fun strip(s: String): String = s.trim { it.isWhitespace() }
+fun strip(s: String): String {
+    return s.trim { it.isWhitespace() }
+}
