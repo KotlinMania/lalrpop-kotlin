493db50615facebdc2abfd930e22356029be2ee6
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 10:07:48 -0700

    Tls.kt: drop RefCell shim for parity

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lr1/Tls.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lr1/Tls.kt
@@ -8,45 +8,31 @@ package io.github.kotlinmania.lalrpop.lr1
 import io.github.kotlinmania.lalrpop.grammar.repr.TerminalSet
 
 private object TERMINALS {
-    private val cell: RefCell<TerminalSet?> = RefCell(null)
-    fun <R> with(op: (RefCell<TerminalSet?>) -> R): R = op(cell)
+    var value: TerminalSet? = null
 }
-
-private class RefCell<T>(private var value: T) {
-    fun borrow(): T = value
-    fun borrowMut(): RefCell<T> = this
-    fun set(other: T) {
-        value = other
-    }
-    fun replace(other: T): T {
-        val old = value
-        value = other
-        return old
-    }
-}
-
-private fun <T> RefCell<T?>.take(): T? = this.replace(null)
 
 class Lr1Tls private constructor(
-    private val oldValue: RefCell<TerminalSet?>,
+    private var oldValue: TerminalSet?,
 ) : AutoCloseable {
 
-    fun drop() {
-        TERMINALS.with { s -> s.borrowMut().set(this.oldValue.take()) }
+    private fun drop() {
+        TERMINALS.value = oldValue
+        oldValue = null
     }
 
     override fun close() = drop()
 
     companion object {
         fun install(terminals: TerminalSet): Lr1Tls {
-            val oldValue = TERMINALS.with { s -> s.borrowMut().replace(terminals) }
-            return Lr1Tls(RefCell(oldValue))
+            val oldValue = TERMINALS.value
+            TERMINALS.value = terminals
+            return Lr1Tls(oldValue)
         }
 
         fun <RET> with(
             op: (TerminalSet) -> RET,
         ): RET {
-            return TERMINALS.with { s -> op(s.borrow() ?: error("LR1 TLS not installed")) }
+            return op(TERMINALS.value ?: error("LR1 TLS not installed"))
         }
     }
 }
