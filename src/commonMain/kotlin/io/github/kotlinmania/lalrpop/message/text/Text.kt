4508d44aa21f64999bec6f9a807b55f02de4ec1d
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 07:13:23 -0700

    message/text/Text.kt: splitWhitespace without regexSyntax

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/message/text/Text.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/message/text/Text.kt
@@ -4,9 +4,7 @@ package io.github.kotlinmania.lalrpop.message.text
 import io.github.kotlinmania.lalrpop.Style
 import io.github.kotlinmania.lalrpop.message.AsciiView
 import io.github.kotlinmania.lalrpop.message.writeChars
-import io.github.kotlinmania.lalrpop.message.horiz.Horiz
 import io.github.kotlinmania.lalrpop.message.message.Content
-import io.github.kotlinmania.lalrpop.message.wrap.Wrap
 
 /**
  * Text to be display. This will be flowed appropriately depending on
@@ -20,15 +18,35 @@ class Text private constructor(
         fun new(text: String): Text = Text(text = text)
     }
 
-    override fun minWidth(): Int = this.text.length
+    override fun minWidth(): Int = this.text.count()
 
     override fun emit(view: AsciiView) {
         view.writeChars(0, 0, this.text.iterator(), Style.new())
     }
 
     override fun intoWrapItems(wrapItems: MutableList<Content>) {
-        for (word in this.text.split(Hir("\\s+")).filter { it.isNotEmpty() }) {
-            wrapItems.add(Text.new(word))
+        for (word in splitWhitespace(this.text)) {
+            wrapItems.add(Text.new(word.toString()))
         }
+    }
+
+    private fun splitWhitespace(input: String): List<CharSequence> {
+        val words = mutableListOf<CharSequence>()
+        var startIndex: Int? = null
+        for (i in input.indices) {
+            val ch = input[i]
+            if (ch.isWhitespace()) {
+                if (startIndex != null) {
+                    words.add(input.subSequence(startIndex, i))
+                    startIndex = null
+                }
+            } else if (startIndex == null) {
+                startIndex = i
+            }
+        }
+        if (startIndex != null) {
+            words.add(input.subSequence(startIndex, input.length))
+        }
+        return words
     }
 }
