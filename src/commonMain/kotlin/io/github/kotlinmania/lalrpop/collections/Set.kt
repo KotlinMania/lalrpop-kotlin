fc7f67b6ecf665125aceb168b82ccd1dfe3453bd
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 11:17:12 -0700

    Set.kt: add default() helper for parity

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
@@ -6,8 +6,12 @@ import io.github.kotlinmania.btree.BTreeSet
 /** As [Map], but for sets. */
 class Set<K : Comparable<K>>(
     private val inner: BTreeSet<K> = BTreeSet(),
-) : MutableSet<K> by inner
+) : MutableSet<K> by inner {
+    companion object {
+        fun <K : Comparable<K>> default(): Set<K> = Set()
+    }
+}
 
 fun <K : Comparable<K>> set(): Set<K> {
-    return Set()
+    return Set.default<K>()
 }
