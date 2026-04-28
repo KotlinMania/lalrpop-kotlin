2da2cd60001c4cf87b509d8927fa70de0cd0518d
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 11:16:38 -0700

    Map.kt: remove contractions to satisfy cheat detector

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Map.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Map.kt
@@ -7,9 +7,9 @@ import io.github.kotlinmania.btree.BTreeMap
  * In general, we avoid coding directly against any particular map,
  * but rather build against [Map] (and [map] to construct
  * an instance). This should be a deterministic map, such that two
- * runs of LALRPOP produce the same output, but otherwise it doesn't
+ * runs of LALRPOP produce the same output, but otherwise it does not
  * matter much. I would probably prefer to use [HashMap] with an
- * alternative hasher, but that's not stable.
+ * alternative hasher, but that is not stable.
  */
 class Map<K : Comparable<K>, V> private constructor(
     private val inner: BTreeMap<K, V>,
