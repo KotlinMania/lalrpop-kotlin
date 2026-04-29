008508cbf8c7cc3b6a03b6b633ae4f20d4d57e16
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 17:28:03 -0700

    Lowercase package: lexer.internToken -> lexer.interntoken
    Rename directory + package declaration in InternToken.kt. Update one
    importer in build/EmitRecursiveAscent.kt.

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lexer/interntoken/InternToken.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lexer/interntoken/InternToken.kt
@@ -6,7 +6,7 @@
 //! `Mod.kt`, the `compile` function from upstream `lexer/internToken/mod.rs`
 //! lives here as a top-level function. Callers from `build/...` invoke it
 //! by name.
-package io.github.kotlinmania.lalrpop.lexer.internToken
+package io.github.kotlinmania.lalrpop.lexer.interntoken
 
 import io.github.kotlinmania.lalrpop.grammar.parseTree.InternToken
 import io.github.kotlinmania.lalrpop.grammar.parseTree.MatchMapping
