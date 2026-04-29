092e1c7190e6f595fd0043e3415f0aaed7b7aecd
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 17:11:52 -0700

    Re.kt: add Regex/RegexError typealiases per upstream pub type decls

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lexer/re/Re.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lexer/re/Re.kt
@@ -1,5 +1,5 @@
 // port-lint: source lexer/re/mod.rs
-//! A parser and representation of regular expressions.
+// A parser and representation of regular expressions.
 package io.github.kotlinmania.lalrpop.lexer.re
 
 import io.github.kotlinmania.lalrpop.regexSyntax.Hir
@@ -7,19 +7,23 @@ import io.github.kotlinmania.lalrpop.regexSyntax.ParserBuilder
 import io.github.kotlinmania.lalrpop.regexSyntax.RegexSyntaxError
 import io.github.kotlinmania.lalrpop.regexSyntax.escape as regexSyntaxEscape
 
+typealias Regex = Hir
+typealias RegexError = RegexSyntaxError
 
 /** Convert a string literal into a parsed regular expression. */
-fun parseLiteral(s: String): Hir =
-    parseRegex(regexSyntaxEscape(s)).getOrElse {
-        throw RuntimeException("failed to parse literal regular expression")
+fun parseLiteral(s: String): Regex {
+    return parseRegex(regexSyntaxEscape(s)).getOrElse {
+        error("failed to parse literal regular expression")
     }
+}
 
 /** Parse a regular expression like `a+` etc. */
-fun parseRegex(s: String): Result<Hir> {
+fun parseRegex(s: String): Result<Regex> {
     val enableUnicode = true
-    return ParserBuilder()
+    val expr = ParserBuilder()
         .utf8(enableUnicode)
         .unicode(enableUnicode)
         .build()
         .parse(s)
+    return expr
 }
