c19a7cd382f5fe84820dc069046f69118cde34b9
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 22:10:54 -0700

    Set.kt: expand single-expression set() to block body for line parity

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Set.kt
@@ -6,4 +6,6 @@ import io.github.kotlinmania.btree.BTreeSet
 /** As [Map], but for sets. */
 typealias Set<K> = BTreeSet<K>
 
-fun <K : Comparable<K>> set(): Set<K> = BTreeSet.default()
+fun <K : Comparable<K>> set(): Set<K> {
+    return BTreeSet.default<K>()
+}
