1daa689a5d7bae820bf9b80af66469d5f5b87aa9
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-28 19:06:55 -0700

    Map.kt: faithful translation as typealias to BTreeMap

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Map.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/collections/Map.kt
@@ -7,12 +7,10 @@ import io.github.kotlinmania.btree.BTreeMap
  * In general, we avoid coding directly against any particular map,
  * but rather build against [Map] (and [map] to construct
  * an instance). This should be a deterministic map, such that two
- * runs of LALRPOP produce the same output, but otherwise it does not
- * matter much. I would probably prefer to use [HashMap] with an
- * alternative hasher, but that is not stable.
+ * runs of LALRPOP produce the same output, but otherwise it doesn't
+ * matter much. I'd probably prefer to use `HashMap` with an
+ * alternative hasher, but that's not stable.
  */
 typealias Map<K, V> = BTreeMap<K, V>
 
-fun <K : Comparable<K>, V> map(): Map<K, V> {
-    return Map<K, V>()
-}
+fun <K : Comparable<K>, V> map(): Map<K, V> = BTreeMap.default()
