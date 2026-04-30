01f3d8c677402e11393d177f3fa4768763c01d87
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-29 21:12:36 -0700

    ParseTable.kt: import State/TokenSet from lr1 package, not nonexistent lr1.core

--- src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lr1/codegen/ParseTable.kt
+++ src/commonMain/kotlin/io/github/kotlinmania/lalrpop/lr1/codegen/ParseTable.kt
@@ -16,7 +16,8 @@ import io.github.kotlinmania.lalrpop.grammar.parsetree.TerminalString
 import io.github.kotlinmania.lalrpop.grammar.repr.TypeRepr
 import io.github.kotlinmania.lalrpop.grammar.repr.WhereClause
 import io.github.kotlinmania.lalrpop.lr1.Token
-import io.github.kotlinmania.lalrpop.lr1.core.State
+import io.github.kotlinmania.lalrpop.lr1.State
+import io.github.kotlinmania.lalrpop.lr1.TokenSet
 import io.github.kotlinmania.lalrpop.rust.RustWrite
 import io.github.kotlinmania.lalrpop.rust.rust
 import io.github.kotlinmania.lalrpop.tls.Tls
